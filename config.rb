b8ec599b949e57d94c20211a6e4953b3926d970a
Craig <craigjope@gmail.com>
2021-06-13 18:15:19 -0600

    blog setup

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/cjope/"
+http_images_path = "/cjope/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/cjope/fonts"
+css_dir = "public/cjope/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
